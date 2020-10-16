4655d2d42521b10a4a23555874ccd01b49ce1c10
Ubuntu <ubuntu@ip-172-31-10-132.us-west-2.compute.internal>
2020-10-16 12:57:31 +0000

    Updated yarn with port

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0abe908062157f15d569833228d669ee.js"
+  "/precache-manifest.9748ca4e80c15aebe6171cf62233c988.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
