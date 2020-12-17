7581ceb02086bbbe322bd6ee8451f01120ecc6c0
Sarmad <sarmad.faheem@ideofuzion.comgit config --global user.email sarmad.faheem@ideofuzion.com>
2020-12-17 16:22:12 +0500

    change in network

--- src/constants/abi/config.js
+++ src/constants/abi/config.js
@@ -1,7 +1,8 @@
 const Config = {
-    chainId: 42,
-    rpcUrl: 'https://kovan.infura.io/v3/a4273886253a4c01b2e41cbfeb190ccd',
-    // rpcUrl:  'https://mainnet.eth.aragon.network/'
+    // chainId: 42,
+    // rpcUrl: 'https://kovan.infura.io/v3/a4273886253a4c01b2e41cbfeb190ccd',
+    chainId: 1,
+    rpcUrl:  'https://mainnet.eth.aragon.network/'
   };
   
   export default Config;
