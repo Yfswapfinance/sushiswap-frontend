6414337659cd3548b9d4c43be4d364df240fedc2
talhamalik883 <talhamalik883@gmail.com>
2021-02-11 20:31:22 +0500

    updated masterchef and yfbtc address

--- src/constants/tokenAddresses.ts
+++ src/constants/tokenAddresses.ts
@@ -2,9 +2,9 @@ export const sushi = '0x0e2298e3b3390e3b945a5456fbf59ecc3f55da16'
 export const sushiv2 = '0xaba8cac6866b83ae4eec97dd07ed254282f6ad8a'
 export const sushiAddress = '0x43a7903E3a839a67192151eE300e11198985E54b'
 // export const masterChefAddress = '0x245A074cA9814fB46A21562bC70fAB92F8A3F779'
-export const masterChefAddress = '0xfd9fA3ca546D40FE4cF3AA4B016d9B7f1a5432f6'
+export const masterChefAddress = '0x1fDf483A5B2f5524F305BbE08F56F478F5FBeB76'
 export const univ2 = '0xe97dece9f7c817fa09594abf14b040caf8c7f3db'
-export const yfbtc = '0x907FAe6B871E0d1feb259F171E06E2695B9cbD65'
+export const yfbtc = '0xff034d12353867fc4228f4ae3e689cd6dcaad120'
 
 
 // export const masterChefAddress = '0x38c6a7dbb996a12861d7143dfb69e8ed840c7f75'
