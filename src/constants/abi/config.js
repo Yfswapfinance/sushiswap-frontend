abd41ef04643e7ed5977a3083ffe6209c5c5f64d
Sarmad_faheem <sarmad.faheem@ideofuzion.comgit config --global user.email sarmad.faheem@ideofuzion.com>
2021-02-11 13:47:14 +0500

    network change to mainnet

--- src/constants/abi/config.js
+++ src/constants/abi/config.js
@@ -1,7 +1,7 @@
 const Config = {
-    chainId: 42,
-    rpcUrl: 'https://kovan.infura.io/v3/a4273886253a4c01b2e41cbfeb190ccd',
-    // rpcUrl:  'https://mainnet.eth.aragon.network/'
+    chainId: 1,
+    // rpcUrl: 'https://kovan.infura.io/v3/a4273886253a4c01b2e41cbfeb190ccd',
+    rpcUrl:  'https://mainnet.eth.aragon.network/'
   };
   
   export default Config;
--- src/constants/tokenAddresses.ts
+++ src/constants/tokenAddresses.ts
@@ -5,3 +5,7 @@ export const sushiAddress = '0x43a7903E3a839a67192151eE300e11198985E54b'
 export const masterChefAddress = '0xfd9fA3ca546D40FE4cF3AA4B016d9B7f1a5432f6'
 export const univ2 = '0xe97dece9f7c817fa09594abf14b040caf8c7f3db'
 export const yfbtc = '0x907FAe6B871E0d1feb259F171E06E2695B9cbD65'
+
+
+// export const masterChefAddress = '0x38c6a7dbb996a12861d7143dfb69e8ed840c7f75'
+// export const yfbtc = '0xff034d12353867fc4228f4ae3e689cd6dcaad120'
